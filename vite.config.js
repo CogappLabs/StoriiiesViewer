c64a26394afede68afee36b7925297220b0b3eb9
jonw-cogapp <jonw@cogapp.com>
2023-07-12 20:48:07 +0100

    Install vite; define files and exports

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+// vite.config.js
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      fileName: "index",
+      formats: ["es"],
+    },
+  },
+});
