883896ef71d24b11aeccaaaddce16ac7941e7672
jonw-cogapp <jonw@cogapp.com>
2023-07-17 18:33:23 +0100

    Get OSD and manifesto working

--- src/demo.ts
+++ src/demo.ts
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   new StoriiiesViewer({
     container,
-    manifest: "https://storiiies.github.io/storiiies-viewer/manifest.json",
+    manifestUrl:
+      "https://cwp4ao9r10.execute-api.us-east-1.amazonaws.com/dev/manifest/d58788314ce040d6a7288a613476e289/manifest.json",
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,59 @@
+import { loadManifest, Manifest } from "manifesto.js";
+import OpenSeadragon from "openseadragon";
 interface StoriiiesViewerConfig {
-  container: Element | string | null;
-  manifest: string;
+  container: HTMLElement | string | null;
+  manifestUrl: string;
 }
 
 export default class StoriiiesViewer {
-  private containerEl: Element | null;
+  private containerEl: HTMLElement | null;
+  private manifestUrl: string = "";
+  public infoJson: string | null = "";
+  public label: string | null = "";
+  public viewer: OpenSeadragon.Viewer | null = null;
 
   constructor(config: StoriiiesViewerConfig) {
-    // Convert string to HTMLElement
+    // Normalise the config container
     if (typeof config.container === "string") {
       this.containerEl = document.querySelector(config.container);
     } else {
       this.containerEl = config.container;
     }
-
+    this.manifestUrl = config.manifestUrl;
     this.init();
   }
 
   /**
    * Initialize the viewer
    */
-  private init() {
+  private async init() {
+    const rawManifest = await loadManifest(this.manifestUrl);
+    const manifest = new Manifest(rawManifest);
+
+    this.label = manifest.getLabel().getValue();
+
+    this.infoJson = manifest
+      .getSequenceByIndex(0)
+      .getCanvases()[0]
+      .getImages()[0]
+      .getResource()
+      .getServices()[0]
+      .getInfoUri();
+
     if (this.containerEl) {
-      this.containerEl.innerHTML = "Storiiies Viewer goes here";
+      this.containerEl.style.height = "80vh";
+      this.containerEl.style.width = "80vw";
     }
+
+    this.viewer = OpenSeadragon({
+      element: this.containerEl ?? undefined,
+      tileSources: [this.infoJson],
+      crossOriginPolicy: "Anonymous",
+      showSequenceControl: false,
+      showHomeControl: false,
+      showZoomControl: false,
+      showFullPageControl: false,
+      visibilityRatio: 0.3,
+    });
   }
 }
