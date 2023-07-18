b92c18d0f6a13e177b3d42f7a8ca7a58036176e8
jonw-cogapp <jonw@cogapp.com>
2023-07-18 16:24:41 +0100

    Output label and add accessible text

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { loadManifest, Manifest } from "manifesto.js";
 import OpenSeadragon from "openseadragon";
 interface StoriiiesViewerConfig {
-  container: HTMLElement | string | null;
+  container: Element | HTMLElement | string;
   manifestUrl: string;
 }
 
@@ -11,24 +11,36 @@ export default class StoriiiesViewer {
   public infoJson: string | null = "";
   public label: string | null = "";
   public viewer: OpenSeadragon.Viewer | null = null;
+  public infoArea: HTMLElement | null = null;
+  public instanceId: number;
 
   constructor(config: StoriiiesViewerConfig) {
+    this.instanceId = document.querySelectorAll(".storiiies-viewer").length;
+
     // Normalise the config container
     if (typeof config.container === "string") {
       this.containerEl = document.querySelector(config.container);
     } else {
-      this.containerEl = config.container;
+      // Minor wrinkle around if the container is an Element or HTMLElement
+      // It should be safe to cast it to HTMLElement
+      this.containerEl = config.container as HTMLElement;
     }
+
     this.manifestUrl = config.manifestUrl;
-    this.init();
+
+    this.initViewer().then(() => {
+      this.insertInfoArea();
+    });
   }
 
   /**
    * Initialize the viewer
    */
-  private async init() {
+  private async initViewer() {
     const rawManifest = await loadManifest(this.manifestUrl);
     const manifest = new Manifest(rawManifest);
+
+    this.containerEl?.classList.add("storiiies-viewer");
 
     this.label = manifest.getLabel().getValue();
 
@@ -55,5 +67,25 @@ export default class StoriiiesViewer {
       showFullPageControl: false,
       visibilityRatio: 0.3,
     });
+    this.viewer.canvas.ariaLabel = "Storiiies viewer";
+    this.viewer.canvas.role = "application";
+    this.viewer.element.insertAdjacentHTML(
+      "afterbegin",
+      `<p style="display:none;" id="storiiies-viewer-${this.instanceId}-description">Drag with your mouse or the arrorw keys, and zoom with scroll or <kbd aria-label="plus">+</kbd> and <kbd aria-label="minus">-</kbd></p>`,
+    );
+    this.viewer.canvas.setAttribute(
+      "aria-describedby",
+      `storiiies-viewer-${this.instanceId}-description`,
+    );
+  }
+
+  /**
+   * Create area for label, annotations and controls
+   */
+  private insertInfoArea() {
+    this.infoArea = document.createElement("div");
+    this.viewer?.element.insertAdjacentElement("afterend", this.infoArea);
+    this.infoArea.classList.add("info-area");
+    this.infoArea.innerText = this.label ?? "";
   }
 }
